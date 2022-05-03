136275de2a274ee99f4cc90e821f0c91b9eb9fec
Carlos Echeverria <carlos.echeverria@kriptops.com>
2022-05-03 13:11:50 -0500

    Cambios para nueva estructura

--- src/main/java/com/kriptops/n98aidbuilder/N98AidBuilder.java
+++ src/main/java/com/kriptops/n98aidbuilder/N98AidBuilder.java
@@ -4,13 +4,144 @@
  */
 package com.kriptops.n98aidbuilder;
 
+import com.kriptops.n98aidbuilder.tlv.BerTag;
+import com.kriptops.n98aidbuilder.tlv.BerTlv;
+import com.kriptops.n98aidbuilder.tlv.BerTlvBuilder;
+import com.kriptops.n98aidbuilder.tlv.HexUtil;
+
 /**
  *
  * @author carlos
  */
 public class N98AidBuilder {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    /**
+     * Construye AID para contactless
+     * 
+     * @param kty
+     * @param tty
+     * @param aid
+     * @param asi
+     * @param config
+     * @return 
+     */
+    public String buildAID(
+            String kty,
+            String tty,
+            String aid,
+            String asi,
+            BerTlv... config
+    ) {
+        var data = HexUtil.toHexString(
+                new BerTlvBuilder(
+                        new BerTlv("DF808000", kty),
+                        new BerTlv("9C", tty),
+                        new BerTlv("9F06", aid),
+                        new BerTlv("DF808060", asi),
+                        new BerTlv(
+                                new BerTag("DF808002"),
+                                new BerTlvBuilder(config).buildArray()
+                        )
+                ).buildArray());
+
+        var length = "0000" + Integer.toString(data.length() / 2, 16);
+        length = length.substring(length.length() - 4);
+        data = (length + data).toUpperCase();
+        return data;
+    }
+    
+    /**
+     * Construye AID para contactless
+     * 
+     * @param kty
+     * @param tty
+     * @param aid
+     * @param asi
+     * @param config
+     * @return 
+     */
+    public String buildAID(
+            String kty,
+            String tty,
+            String aid,
+            String asi,
+            String config
+    ) {
+        var data = HexUtil.toHexString(
+                new BerTlvBuilder(
+                        new BerTlv("DF808000", kty),
+                        new BerTlv("9C", tty),
+                        new BerTlv("9F06", aid),
+                        new BerTlv("DF808060", asi),
+                        new BerTlv(
+                                new BerTag("DF808002"),
+                                HexUtil.parseHex(config)
+                        )
+                ).buildArray());
+
+        var length = "0000" + Integer.toString(data.length() / 2, 16);
+        length = length.substring(length.length() - 4);
+        data = (length + data).toUpperCase();
+        return data;
+    }
+    
+    /**
+     * Construye el AID para contact.
+     * 
+     * @param aid valor del aid a utilizar
+     * @param asi indica si es match exacto o inexacto
+     * @param config la configuracion del AID que se extrae del tool o una config manual
+     * @return 
+     */
+    public String buildAID(
+            String aid,
+            String asi,
+            BerTlv... config
+    ) {
+        var data = HexUtil.toHexString(
+                new BerTlvBuilder(
+                        new BerTlv("DF808000", "00"),
+                        new BerTlv("9F06", aid),
+                        new BerTlv("DF808060", asi),
+                        new BerTlv(
+                                new BerTag("DF808002"),
+                                new BerTlvBuilder(config).buildArray()
+                        )
+                ).buildArray());
+
+        var length = "0000" + Integer.toString(data.length() / 2, 16);
+        length = length.substring(length.length() - 4);
+        data = (length + data).toUpperCase();
+        return data;
+    }
+    
+    /**
+     * Construye el AID para contact.
+     * 
+     * @param aid valor del aid a utilizar
+     * @param asi indica si es match exacto o inexacto
+     * @param config la configuracion del AID que se extrae del tool o una config manual
+     * @return 
+     */
+    public String buildAID(
+            String aid,
+            String asi,
+            String config
+    ) {
+        var data = HexUtil.toHexString(
+                new BerTlvBuilder(
+                        new BerTlv("DF808000", "00"),
+                        new BerTlv("9F06", aid),
+                        new BerTlv("DF808060", asi),
+                        new BerTlv(
+                                new BerTag("DF808002"),
+                                HexUtil.parseHex(config)
+                        )
+                ).buildArray());
+
+        var length = "0000" + Integer.toString(data.length() / 2, 16);
+        length = length.substring(length.length() - 4);
+        data = (length + data).toUpperCase();
+        return data;
     }
 }
--- src/test/java/NewEmptyJUnitTest.java
+++ src/test/java/NewEmptyJUnitTest.java
@@ -3,20 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import com.kriptops.n98aidbuilder.N98AidBuilder;
 import com.kriptops.n98aidbuilder.tlv.BerTag;
 import com.kriptops.n98aidbuilder.tlv.BerTlv;
 import com.kriptops.n98aidbuilder.tlv.BerTlvBuilder;
 import com.kriptops.n98aidbuilder.tlv.BerTlvParser;
-import com.kriptops.n98aidbuilder.tlv.BerTlvs;
 import com.kriptops.n98aidbuilder.tlv.HexUtil;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.ResourceBundle;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -46,13 +42,9 @@ public class NewEmptyJUnitTest {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    @Test
-    public void hello() {
+
+    public void analize(String AID) {
         var bundle = ResourceBundle.getBundle("tagsofplatform");
-        var AID = "00A6DF80800001009C01009F0607A0000000031010DF8080600101DF80800281879F01061234567890129F090200029F160F3132333435363738393031323334359F150212349F4E0D54657374204D65726368616E749F1C0846726F6E743132339F1A0206049F3501229F3303E0F8E89F4005F000F0F001DF80806106000000100000DF808020050000000000DF808021050000000000DF8080220500000000009F1B0400000000";
         AID = AID.substring(4);
         var parser = new BerTlvParser();
         var parsedLvl1 = parser.parse(HexUtil.parseHex(AID));
@@ -75,60 +67,14 @@ public class NewEmptyJUnitTest {
     }
 
     @Test
-    public void aidsVisa() {
-        System.out.println("Test AIDS Visa");
-        for (var aid : new String[]{"A0000000031010", "A0000000032010", "A0000000033010"}) {
-            var data = buildAID(
-                    "00",
-                    "00",
-                    aid,
-                    "01",
-                    new BerTlv("9F01", "123456789012"),
-                    new BerTlv("9F09", "0002"),
-                    new BerTlv("9F16", "313233343536373839303132333435"),
-                    new BerTlv("9F15", "1234"),
-                    new BerTlv("9F4E", "54657374204D65726368616E74"),
-                    new BerTlv("9F1C", "46726F6E74313233"),
-                    new BerTlv("9F1A", "0604"),
-                    new BerTlv("9F35", "21"),
-                    new BerTlv("9F33", "6048C8"),
-                    new BerTlv("9F40", "F000F02001"),
-                    new BerTlv("DF808061", "000000048000"),
-                    new BerTlv("DF808020", "0010000000"),
-                    new BerTlv("DF808021", "DC4000A800"),
-                    new BerTlv("DF808022", "DC4004F800"),
-                    new BerTlv("DF808026", "9F0206"),
-                    new BerTlv("DF808027", "9F3704"),
-                    new BerTlv("9F1B", "00000000"),
-                    new BerTlv("DF80802C", "000000015000")
-            );
-            System.out.println(data);
-        }
+    public void AMEXCTLS () {
+        var aid = new N98AidBuilder().buildAID("04", "00", "A000000025", "01", "9F01061234567890129F090200019F160F3132333435363738393031323334359F150212349F4E0D54657374204D65726368616E749F1C0846726F6E743132339F1A0200009F1E083832313030303031DF808061010E9F3501219F3303E0F8C89F1B0400000000DF808020050000000000DF808021050000000000DF808022050000000000DF80802A06000099999900DF80802B06000000000000DF80802C06000000015000");
+        System.out.println(aid);
+        analize(aid);
     }
 
-    public String buildAID(
-            String kty,
-            String tty,
-            String aid,
-            String asi,
-            BerTlv... config
-    ) {
-        var data = HexUtil.toHexString(
-                new BerTlvBuilder(
-                        new BerTlv("DF808000", kty),
-                        new BerTlv("9C", tty),
-                        new BerTlv("9F06", aid),
-                        new BerTlv("DF808060", asi),
-                        new BerTlv(
-                                new BerTag("DF808002"),
-                                new BerTlvBuilder(config).buildArray()
-                        )
-                ).buildArray());
-
-        var length = "0000" + Integer.toString(data.length() / 2, 16);
-        length = length.substring(length.length() - 4);
-        data = (length + data).toUpperCase();
-        return data;
+    
+    @Test
+    public void DINERSCTLS() {
     }
-
 }
